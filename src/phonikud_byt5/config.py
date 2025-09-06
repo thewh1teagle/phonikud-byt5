28cae98ef8304470a7b70167e093cb73dfe48805
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2025-09-06 18:18:07 +0000

    train config

--- src/phonikud_byt5/config.py
+++ src/phonikud_byt5/config.py
@@ -14,14 +14,14 @@ class TrainArgs(Tap):
     # Training
     batch_size: int = 8
     learning_rate: float = 5e-5
-    num_epochs: int = 10
+    num_epochs: int = 999999
     val_split: float = 0.01  # 1% for validation
     split_seed: int = 42
     device: Literal["mps", "cuda", "cpu"] = "cuda"  # Device to use: "mps", "cuda", "cpu"
     
     # Evaluation & Saving
-    eval_steps: int = 10  # How often to evaluate (and save best/last models)
-    logging_steps: int = 10  # How often to log training metrics
+    eval_steps: int = 250  # How often to evaluate (and save best/last models)
+    logging_steps: int = 50  # How often to log training metrics
     
     # Wandb configuration (for experiment tracking)
     wandb_entity: str = "Phonikud"  # Team or username for Weights & Biases
